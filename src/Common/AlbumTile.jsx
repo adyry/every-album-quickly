e4b46aa7b58198ea9a86e4ebb6a3d3b4bbcaeb5f
adyry <adrian.kowalczewski@gmail.com>
2023-02-10 12:17:47 +0000

    Add all

--- src/Common/AlbumTile.jsx
+++ src/Common/AlbumTile.jsx
@@ -1,30 +1,48 @@
 import { useDispatch } from "react-redux";
 import { selectTrack } from "../store/selectedSlice";
 import TrackTile from "./TrackTile";
+import { Button } from "@mui/material";
 
 const AlbumTile = ({ images, tracks, name, artists, label }) => {
   const dispatch = useDispatch();
 
+  const addAll = () => {
+    tracks.items.forEach((track) => {
+      dispatch(selectTrack({ uri: track.uri, checked: true }));
+    });
+  };
+
   const onAlbumKeyPress = (e) => {
     if (e.key === "a") {
-      tracks.items.forEach((track) => {
-        dispatch(selectTrack({ uri: track.uri, checked: true }));
-      });
+      addAll();
     }
   };
 
   return (
     <div className="album-tile" onKeyPress={onAlbumKeyPress}>
-      <div>
-        {artists.map(
-          ({ name, id, external_urls: { spotify: artist_link } }) => name + " "
-        )}
-        - {name}
+      <div className="heading">
+        <div className="cover">
+          <img src={images[2].url} alt="cover" />{" "}
+        </div>
+        <div className="description">
+          <div>
+            {artists.map(
+              ({ name, id, external_urls: { spotify: artist_link } }) =>
+                name + " "
+            )}
+            - {name}
+          </div>
+          <div>label: {label}</div>
+        </div>
       </div>
-      <div>label: {label}</div>
-      <div>
-        <img src={images[2].url} alt="cover" />{" "}
-      </div>
+      <Button
+        onClick={addAll}
+        size="small"
+        variant="contained"
+        color="secondary"
+      >
+        all
+      </Button>
       <div>
         {tracks.items.map((track) => (
           <TrackTile key={track.uri} {...track} />
